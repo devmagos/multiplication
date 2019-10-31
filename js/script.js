a73a49d25fbf342bf8a6252402459a4bd6381d72
Omakoji Danlami Enejoh <ernestmoi@yahoo.com>
2019-10-31 17:41:50 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,175 @@
+const displayData1 = () => {
+    let a = 1;
+    let i = 1;
+    let result = " ";
+    
+    for (i = 1; i <= 12; i++){
+
+    let c = parseInt(a) * parseInt(i);
+
+    result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+
+    document.getElementById("display").innerHTML = result; 
+    
+}
+}
+
+const displayData2 = () => {
+    let a = 2;
+    let i = 1;
+    let result = " ";
+    for (i = 1; i <= 12; i++){
+
+    let c = parseInt(a) * parseInt(i);
+
+    result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+ 
+    document.getElementById("display").innerHTML = result; 
+   }
+}
+
+const displayData3 = () => {
+        let a = 3;
+        let i = 1;
+        let result = " ";
+        for (i = 1; i <= 12; i++){
+    
+        let c = parseInt(a) * parseInt(i);
+    
+        result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+     
+        document.getElementById("display").innerHTML = result; 
+    }
+}
+
+const displayData4 = () => {
+            let a = 4;
+            let i = 1;
+            let result = " ";
+            for (i = 1; i <= 12; i++){
+        
+            let c = parseInt(a) * parseInt(i);
+        
+            result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+         
+            document.getElementById("display").innerHTML = result; 
+        }
+   }
+
+   const displayData5 = () => {
+    let a = 5;
+    let i = 1;
+    let result = " ";
+    for (i = 1; i <= 12; i++){
+
+    let c = parseInt(a) * parseInt(i);
+
+    result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+ 
+    document.getElementById("display").innerHTML = result; 
+ }
+}
+
+const displayData6 = () =>{
+    let a = 6;
+    let i = 1;
+    let result = " ";
+    for (i = 1; i <= 12; i++){
+
+    let c = parseInt(a) * parseInt(i);
+
+    result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+ 
+    document.getElementById("display").innerHTML = result; 
+   }                    
+ }
+                    
+
+
+ const displayData7 = () =>{
+    let a = 7;
+    let i = 1;
+    let result = " ";
+    for (i = 1; i <= 12; i++){
+
+    let c = parseInt(a) * parseInt(i);
+
+    result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+ 
+    document.getElementById("display").innerHTML = result; 
+   }                     
+}
+
+const displayData8 = () =>{
+        let a = 8;
+        let i = 1;
+        let result = " ";
+        for (i = 1; i <= 12; i++){
+    
+        let c = parseInt(a) * parseInt(i);
+    
+        result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+     
+        document.getElementById("display").innerHTML = result; 
+       }    
+                            
+   }
+
+   const displayData9 = () => {
+        let a = 9;
+        let i = 1;
+        let result = " ";
+        for (i = 1; i <= 12; i++){
+    
+        let c = parseInt(a) * parseInt(i);
+    
+        result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+     
+        document.getElementById("display").innerHTML = result; 
+       }    
+                                
+    }
+
+    const displayData10 = () =>{
+            let a = 10;
+            let i = 1;
+            let result = " ";
+            for (i = 1; i <= 12; i++){
+        
+            let c = parseInt(a) * parseInt(i);
+        
+            result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+         
+            document.getElementById("display").innerHTML = result; 
+           }    
+                                    
+    }
+
+    const displayData11 = () => {
+            let a = 11;
+            let i = 1;
+            let result = " ";
+            for (i = 1; i <= 12; i++){
+        
+            let c = parseInt(a) * parseInt(i);
+        
+            result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+         
+            document.getElementById("display").innerHTML = result; 
+           }    
+                                    
+    }
+    
+    const displayData12 = () =>{
+            let a = 12;
+            let i = 1;
+            let result = " ";
+            for (i = 1; i <= 12; i++){
+        
+            let c = parseInt(a) * parseInt(i);
+        
+            result += a + " " + " x " + " " + i + " = " + " " +c + "<br>" +  "<br>";
+         
+            document.getElementById("display").innerHTML = result; 
+           }             
+    }
